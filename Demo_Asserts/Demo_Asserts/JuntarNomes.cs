bde38dccc44843543a69d4ff3f39c6a480453994
Rafael <rafaelsantos.rsdn@gmail.com>
2018-07-30 20:54:02 -0300

    Commit Assert Strings

---
+++ Demo_Asserts/Demo_Asserts/JuntarNomes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Asserts
+{
+    public class JuntarNomes
+    {
+
+        public string Juntar(string nome, string sobrenome)
+        {
+            return nome + " " + sobrenome;
+        }
+
+    }
+}
---
+++ Demo_Asserts/Demo_Asserts_Tests/Igualdade/JuntarNomes.cs
@@ -0,0 +1,39 @@
+﻿using Demo_Asserts;
+using NUnit.Framework;
+
+namespace Demo_Asserts_Tests.Igualdade
+{
+    [TestFixture]
+    public class JuntarNomesTests
+    {
+        /*Método de teste - finalidade: comparar strings juntas*/
+        [Test]
+        public void DevoJuntarNomeCompleto()
+        {
+            var sut = new JuntarNomes();
+            var nomeCompleto = sut.Juntar("Rafael", "Nascimento");
+            Assert.That(nomeCompleto, Is.EqualTo("Rafael Nascimento"));
+        }
+
+        /*Método de teste - finalidade: comparar string ignorando maiúsculas e minúsculas*/
+        [Test]
+        public void DevoJuntarNomeCompleto_CaseSensitive()
+        {
+            var sut = new JuntarNomes();
+            var nomeCompleto = sut.Juntar("Rafael", "Nascimento");
+            Assert.That(nomeCompleto, Is.EqualTo("Rafael Nascimento").IgnoreCase);
+        }
+
+        /*Método de teste - finalidade: comparar string diferentes*/
+        [Test]
+        public void DevoJuntarNomeCompleto_NaoIgual()
+        {
+            var sut = new JuntarNomes();
+            var nomeCompleto = sut.Juntar("Rafael", "Nascimento");
+            Assert.That(nomeCompleto, Is.Not.EqualTo("Rodolfo Silva").IgnoreCase);
+        }
+
+
+
+    }
+}
